55a62c8e5386fa24cb760f6b664018714f96d3a3
Rennan Coelho <141645170+rennan-coelho@users.noreply.github.com>
2023-09-20 11:49:12 -0300

    Testando uns exemplos

---
+++ phyton-teste/aula10.1.py
@@ -0,0 +1,4 @@
+nota1 = float(input('digite sua primeira nota: '))
+nota2 = float(input('digite sua segunda nota: '))
+m = (nota1 + nota2) / 2
+print('Sua media foi de {:.1}.' .format(m))
---
+++ phyton-teste/aula10.py
@@ -0,0 +1,6 @@
+n = str(input('Digite seu nome: '))
+if n == 'Rennan':
+    print('Que nome lindo!')
+else:
+    print('Seu nome é tão normal.')    
+print('Bom dia {}!' .format(n))    
